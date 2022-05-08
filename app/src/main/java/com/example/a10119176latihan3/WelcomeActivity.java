442e9db60e4eb816d00691a90485f4122cac48d0
Muhammad Fauzan Fadhlulbarr <muhammadfauzanfadhlulbarr@gmail.com>
2022-05-08 16:14:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10119176latihan3/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.example.a10119176latihan3;
+
+import static com.example.a10119176latihan3.DoneActivity.ID_EXTRA_MSG_EXIT;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+//Muhammad Fauzan Fadhlulbarr | 08 Mei 2022 | IF10k | 10119176
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //mendeklarasikan butterknife di activity
+        ButterKnife.bind(this);
+
+        // logika untuk menampung nilai putekstra dari kelas DoneActivity,
+        // agar ketika dihalaman akhir (DoneActivity) ditekan button ok,
+        // Semua activity dapat tertutup juga (finish).
+        if (getIntent().getBooleanExtra(ID_EXTRA_MSG_EXIT, false)) {
+            finish();
+        }
+    }
+    @OnClick(R.id.btnWalkthroughStart)
+    void mulai() {
+        // Perintah pindah halaman
+        Intent intent = new Intent(this, com.example.a10119176latihan3.LoginCodeActivity.class);
+        startActivity(intent);
+    }
+}
